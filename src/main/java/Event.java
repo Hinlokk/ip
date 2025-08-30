6047cf5822a4bb20f294f3b240941da405c92028
Hinlokk <e1121495@u.nus.edu>
2025-08-31 01:12:38 +0800

    Add time/date to Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description, TaskType.EVENT);
-        this.from = from;
-        this.to = to;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + " (from: " + from + " to: " + to + ")";
+        this.from = LocalDate.parse(from); // expects yyyy-mm-dd
+        this.to = LocalDate.parse(to);     // expects yyyy-mm-dd
     }
 
     @Override
     public String toSaveFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+        return "E | " + (isDone ? 1 : 0) + " | " + description + " | " + from + " | " + to;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMM d yyyy");
+        return super.toString() + " (from: " + from.format(fmt)
+                + " to: " + to.format(fmt) + ")";
     }
 }
