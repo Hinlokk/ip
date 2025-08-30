326738e55920e086c90f661e38e4e9b3be9c0464
Hinlokk <e1121495@u.nus.edu>
2025-08-30 20:31:45 +0800

    Level-1

--- src/main/java/Xiaodavid.java
+++ src/main/java/Xiaodavid.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
+
 public class Xiaodavid {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input;
+
         System.out.println("Hello! I'm Xiaodavid!");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true) {
+            input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+
+        }
+        }
     }
-}
+
