512f734e0c4d09f311c63e8d46a1f07317f8835d
Hinlokk <e1121495@u.nus.edu>
2025-08-30 20:40:50 +0800

    Level-2

--- src/main/java/Xiaodavid.java
+++ src/main/java/Xiaodavid.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Xiaodavid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
+        ArrayList<String> list = new ArrayList<>();
 
         System.out.println("Hello! I'm Xiaodavid!");
         System.out.println("What can I do for you?");
@@ -17,9 +19,20 @@ public class Xiaodavid {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")){
+                for(int i = 0; i < list.size();i++) {
+                    System.out.println((i +1) + ". " + list.get(i));
+
+                }
+                continue;
+            }
+
+            list.add(input);
+
+            System.out.println("added: " + input);
 
         }
+        sc.close();
         }
     }
 
